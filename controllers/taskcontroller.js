86515fc54c1e4d55fe3272008bd12b36bcd8ff84
S H M J N Sooriyahetti <janithnilukshan@gmail.com>
2024-11-28 14:05:31 +0530

    updatecontrollers

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,25 +1,50 @@
 const Task = require('../models/taskmodel');
 
 
-const getAlltasks = (req, res)=>{
-    res.send('get all tasks');
+const getAlltasks = async (req, res)=>{
+    try {
+        res.status(201).json('All tasks');
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
 const createTask = async (req, res)=> {
-    const task = await Task.create(req.body)
-    res.status(201).json({task});
+    try {
+        const task = await Task.create(req.body)
+        if(!task){
+            return res.status(404).json({error:'task not found'});
+        }
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
-const getTask = (req, res)=> {
-    res.json({id:req.params.id});
+const getTask = async (req, res)=> {
+    try {
+        res.status(201).json({id:req.params.id});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
-const updateTask = (req, res)=> {
-    res.send('update task');
+const updateTask = async (req, res)=> {
+    try {
+        const {id}= req.params;
+        const task = await Task.findByIdAndUpdate(id, req.body, {new:true, runValidator:true});
+    } catch (error) {
+        res.status(500).json({msg:errror});
+    }
 }
 
-const deleteTask = (req, res)=> {
-    res.send('delet task');
+const deleteTask = async (req, res)=> {
+    try {
+        const {id}= req.params;
+        const task = await Task.findByIdAndDelete(id);
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
 
--- models/taskmodel.js
+++ models/taskmodel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    complete:Boolean
+    name:{type:String,required:[true, 'must provide name'],trim:true, maxlength:[20, 'name cannot be more than 20 characters']},
+    complete:{type:Boolean, default:false,}
 });
 
 module.exports = mongoose.model('Task',TaskSchema);
